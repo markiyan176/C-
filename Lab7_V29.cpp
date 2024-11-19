f5345453677e9b8e51b6b3f1759dbf520f26dc80
markiyan176 <144619934+markiyan176@users.noreply.github.com>
2024-11-19 14:02:40 +0200

    Create Lab7_V29.cpp

---
+++ Lab7_V29.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Введіть кількість елементів масиву: ";
+    cin >> N;
+
+    srand(time(0));
+    
+    vector<double> arr(N);
+    
+    cout << "Масив заповнений випадковими числами:\n";
+    for (int i = 0; i < N; i++) {
+        arr[i] = (rand() % 201 - 100) / 10.0; 
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    double minPositive = numeric_limits<double>::max();
+    int minPositiveIndex = -1;
+    int lastNegativeIndex = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] > 0 && arr[i] < minPositive) {
+            minPositive = arr[i];
+            minPositiveIndex = i;
+        }
+        if (arr[i] < 0) {
+            lastNegativeIndex = i;
+        }
+    }
+
+    if (minPositiveIndex == -1 || lastNegativeIndex == -1) {
+        cout << "Мінімального додатнього або останнього від'ємного елемента не знайдено.\n";
+        return 0;
+    }
+
+
+    int firstPositiveIndex = -1;
+    for (int i = 0; i < N; i++) {
+        if (arr[i] > 0) {
+            firstPositiveIndex = i;
+            break;
+        }
+    }
+
+    double sumBetween = 0;
+    if (firstPositiveIndex < lastNegativeIndex) {
+        for (int i = firstPositiveIndex + 1; i < lastNegativeIndex; i++) {
+            sumBetween += arr[i];
+        }
+    }
+
+    // Виводимо результати
+    cout << "Мінімальний додатний елемент: " << minPositive << " (індекс " << minPositiveIndex << ")\n";
+    cout << "Сума елементів між першим додатнім і останнім від'ємним: " << sumBetween << "\n";
+
+
+    swap(arr[minPositiveIndex], arr[lastNegativeIndex]);
+
+
+    cout << "Масив після перестановки:\n";
+    for (double element : arr) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
