57df6eda389f5872fd3c6d4fdf810a4042378da2
markiyan176 <144619934+markiyan176@users.noreply.github.com>
2024-11-29 11:59:51 +0200

    Update Lab6_V10.cpp

--- Lab6_V10.cpp
+++ Lab6_V10.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>  
-#include <cmath> 
+#include <fstream> // Для роботи з файлами
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 
 using namespace std;
 
@@ -9,21 +10,31 @@ int main() {
     const int SIZE = 10;
     int matrix[SIZE][SIZE];
 
-
     srand(time(0));
 
+    // Відкриття файлу для запису результатів
+    ofstream outputFile("result.txt"); // Назва файлу, куди будуть записані результати
+
+    if (!outputFile) {
+        cerr << "Помилка відкриття файлу для запису!" << endl;
+        return 1;
+    }
+
+    outputFile << "Результат роботи програми\n";
+    outputFile << "Згенерована матриця 10x10:\n";
 
     cout << "Згенерована матриця 10x10:" << endl;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             matrix[i][j] = rand() % 101 - 50; 
             cout << matrix[i][j] << "\t";    
+            outputFile << matrix[i][j] << "\t"; // Запис у файл
         }
         cout << endl;
+        outputFile << endl;
     }
 
     int localMinCount = 0;
-
     for (int i = 1; i < SIZE - 1; i++) {
         for (int j = 1; j < SIZE - 1; j++) {
             int current = matrix[i][j];
@@ -35,7 +46,6 @@ int main() {
     }
 
     int sumAboveDiagonal = 0;
-
     for (int i = 0; i < SIZE; i++) {
         for (int j = i + 1; j < SIZE; j++) {
             sumAboveDiagonal += abs(matrix[i][j]);
@@ -45,6 +55,13 @@ int main() {
     // Виведення результатів
     cout << "Кількість локальних мінімумів: " << localMinCount << endl;
     cout << "Сума модулів елементів вище головної діагоналі: " << sumAboveDiagonal << endl;
+
+    outputFile << "\nКількість локальних мінімумів: " << localMinCount << endl;
+    outputFile << "Сума модулів елементів вище головної діагоналі: " << sumAboveDiagonal << endl;
+
+    outputFile.close(); // Закриття файлу
+
+    cout << "Результати записані у файл 'result.txt'." << endl;
 
     return 0;
 }
